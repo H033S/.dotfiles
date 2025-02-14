4a4df846d1429fe438f8bc720fc8d634a8a6a841
H033S <tonito.nazco@gmail.com>
2025-02-14 10:54:26 -0500

    Made changes on Nvim to Finish NeoTest Config

--- nvim/.config/nvim/lua/h033s/plugins/coding/neo-test.lua
+++ nvim/.config/nvim/lua/h033s/plugins/coding/neo-test.lua
@@ -1,36 +1,59 @@
 return {
+	{
+		"rcasia/neotest-java",
+		ft = "java",
+	},
+	{
+		"nvim-neotest/neotest-python",
+		ft = "python",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+	},
+	{
 
-    "nvim-neotest/neotest",
-    dependencies = {
-        "nvim-neotest/nvim-nio",
-        "nvim-lua/plenary.nvim",
-        "antoinemadec/FixCursorHold.nvim",
-        "nvim-treesitter/nvim-treesitter",
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
 
-        -- Adapters
-        "rcasia/neotest-java",
-        "nvim-neotest/neotest-python",
-    },
+		config = function()
+			local neotest = require("neotest")
 
-    config = function ()
+			neotest.setup({
 
-        local neotest = require('neotest')
+				adapters = {
+					require("neotest-python"),
+					require("neotest-java"),
+				},
+			})
 
-        neotest.setup({
+            vim.keymap.set("n", "<leader>trA", function()
+                neotest.run.run(vim.uv.cwd())
+            end, { desc = "Run All Test Files" })
 
-            adapters = {
-                ["neotest-python"] = {},
-                ["neotest-java"] = {},
-            }
-        })
+			vim.keymap.set("n", "<leader>tr", function()
+				neotest.run.run(vim.fn.expand("%"))
+			end, { desc = "Run File" })
 
-        vim.keymap.set('n', '<leader>tr',  function() neotest.run.run(vim.fn.expand("%")) end ,{desc = 'Run File'})
-        vim.keymap.set('n', '<leader>trA', function() neotest.run.run(vim.uv.cwd()) end ,{desc = 'Run All Test Files'})
-        vim.keymap.set('n', '<leader>tra', function() neotest.run.run() end ,{desc = 'Run Nearest Test'})
-        vim.keymap.set('n', "<leader>ts",  function() neotest.summary.toggle() end, {desc = "Toggle Summary"})
-        vim.keymap.set('n', "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, {desc = "Show Output" })
-        vim.keymap.set('n', "<leader>tO", function() require("neotest").output_panel.toggle() end, {desc = "Toggle Output Panel" })
-        vim.keymap.set('n', "<leader>tS", function() require("neotest").run.stop() end, {desc = "Stop" })
-    end
+			vim.keymap.set("n", "<leader>tra", function()
+				neotest.run.run()
+			end, { desc = "Run Nearest Test" })
+
+			vim.keymap.set("n", "<leader>ts", function()
+				neotest.summary.toggle()
+			end, { desc = "Toggle Summary" })
+
+			vim.keymap.set("n", "<leader>to", function()
+				require("neotest").output.open({ enter = true, auto_close = true })
+			end, { desc = "Show Output" })
+
+			vim.keymap.set("n", "<leader>tS", function()
+				require("neotest").run.stop()
+			end, { desc = "Stop" })
+
+		end,
+	},
 }
-
