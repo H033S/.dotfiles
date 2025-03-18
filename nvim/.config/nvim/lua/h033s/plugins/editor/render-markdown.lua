e65cfe8e3f89aab026a7455e1b8a1566d9a6bb4e
H033S <tonito.nazco@gmail.com>
2025-03-18 19:35:16 -0400

    Added render-markdown in nvim to have a good visual when reading md files

--- nvim/.config/nvim/lua/h033s/plugins/editor/render-markdown.lua
+++ nvim/.config/nvim/lua/h033s/plugins/editor/render-markdown.lua
@@ -1,8 +1,18 @@
 return {
-	"MeanderingProgrammer/render-markdown.nvim",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-		"echasnovski/mini.nvim",
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"echasnovski/mini.nvim",
+		},
+		opts = {},
 	},
-	opts = {},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
 }
